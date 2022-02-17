c1ba7edab828f8080d1486280921476b66db9ea1
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-17 14:39:25 +0300

    styling the homepage

--- run.py
+++ run.py
@@ -3,16 +3,45 @@ import requests
 
 app = Flask(__name__)
 
+ 
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
 @app.route("/")
-def index():
+@app.route("/quote")
+def quote():
     url = "http://quotes.stormconsultancy.co.uk/random.json"
     data = requests.get(url)
     response = data.json()
     quote = response["quote"]
     author = response["author"]
      
-    return render_template("index.html",quote=quote,author=author)   
+    return render_template("quote.html",quote=quote,author=author)   
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
 
 
 if __name__ == '__main__':
