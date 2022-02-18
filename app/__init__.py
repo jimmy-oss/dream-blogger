c10ebb0868e27361c479fc07e2fd5d61be1c4453
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-18 12:23:19 +0300

    blueprinting and configuring the files

--- app/__init__.py
+++ app/__init__.py
@@ -11,7 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
@@ -20,4 +20,11 @@ app.config['MAIL_USERNAME'] = 'kimborne7@gmail.com'
 app.config['MAIL_PASSWORD'] = 'testuser.com'
 mail = Mail(app)
 
-from app import routes
+from app.users.routes import users
+from app.main.routes import main
+from app.posts.routes import posts
+
+
+app.register_blueprint(users)
+app.register_blueprint(main)
+app.register_blueprint(posts)
---
+++ app/main/routes.py
@@ -0,0 +1,29 @@
+from flask import Blueprint  
+from flask import render_template, request, Blueprint
+from app.models import Post
+import requests
+
+main = Blueprint('main',__name__)
+
+@main.route("/")
+@main.route("/quote")
+def quote():
+    url = "http://quotes.stormconsultancy.co.uk/random.json"
+    data = requests.get(url)
+    response = data.json()
+    quote = response["quote"]
+    author = response["author"]
+     
+    return render_template("quote.html",quote=quote,author=author)   
+
+@main.route("/")
+@main.route("/home")
+def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=2)
+    return render_template('home.html', posts=posts)
+
+
+@main.route("/about")
+def about():
+    return render_template('about.html', title='About')
