437265c137575ff7cba83ee403d84e516553d483
jimmy-oss <jimmynjonge51@gmail.com>
2022-02-18 16:04:34 +0300

    rendering migrations file

--- manage.py
+++ manage.py
@@ -1,17 +1,28 @@
-from app import create_app
+from app import create_app,db
 from flask_script import Manager,Server
+from app.models import User
+from flask_migrate import Migrate, MigrateCommand
 
-# Creating app instance
-app = create_app('development')
-
+#Creating app instance
+app = create_app('production')
+# change the above to production when deploying to heruko
 manager = Manager(app)
 manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+migrate.init_app(app, db)
+
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
